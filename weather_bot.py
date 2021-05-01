ced9fb1a75323f724016f581da31aae7ddf198b9
nurilikes <70664956+nurilikes@users.noreply.github.com>
2021-05-01 18:35:10 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -4,7 +4,7 @@ import random
 
 url = 'http://api.openweathermap.org/data/2.5/weather' #open weather url
 api_open_weather = '966cc6ce89188b2bc797546a3487bf55'#ключ open weather api
-api_telegram_token = '1687680321:AAHm7NH2ZsagoBAlcfNSV8U5Wwj0U9jhHK0' #токен telegram api
+api_telegram_token = '1773699578:AAFWpnvES0Zqzky7g1k8iBPHbEh0UF3htnI' #токен telegram api
 
 print("")
 print("initialize") #сообщение в консоль
