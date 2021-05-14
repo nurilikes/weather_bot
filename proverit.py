481455e3d81d1ecc2978dfcfc91fd2c1c7fc1afb
nurilikes <70664956+nurilikes@users.noreply.github.com>
2021-05-14 13:42:23 +0500

    Update proverit.py

--- proverit.py
+++ proverit.py
@@ -4,7 +4,7 @@ import random
 
 url = 'http://api.openweathermap.org/data/2.5/weather' #open weather url
 api_open_weather = '966cc6ce89188b2bc797546a3487bf55'#ключ open weather api
-api_telegram_token = '1773699578:AAFWpnvES0Zqzky7g1k8iBPHbEh0UF3htnI' #токен telegram api
+api_telegram_token = '1629247479:AAHFkYhoIOY1VyaOWm4Y0b70m1DHYEMo05Q' #токен telegram api
 
 print("")
 print("initialize") #сообщение в консоль
